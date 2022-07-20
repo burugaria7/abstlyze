739fd4965a6b89f790c273c820086821707a0b6e
burugaria7 <burugaria@icloud.com>
2022-07-20 15:39:05 +0900

    Fixed bug with line breaks.

--- csv_parser.py
+++ csv_parser.py
@@ -2,10 +2,10 @@ import csv
 
 def add_raw_csv(data):
     filepath = "./data.csv"
-    with open(filepath, 'a') as f:
+    with open(filepath, 'a',newline="") as f:
         writer = csv.writer(f)
         writer.writerows(data)
 
 if __name__ == '__main__':
-    data = [["1","URL","Title","Abstract"]]
+    data = [["Title","URL","Abstract"]]
     add_raw_csv(data)
