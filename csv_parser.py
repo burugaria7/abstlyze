c9411030f9c4893017c20a1ff4166b465c3da19b
burugaria7 <burugaria@icloud.com>
2022-07-21 18:18:03 +0900

    Add code to convert csv to txt

--- csv_parser.py
+++ csv_parser.py
@@ -1,4 +1,5 @@
 import csv
+from csv import reader
 
 def add_raw_csv(data):
     filepath = "./data.csv"
@@ -6,6 +7,20 @@ def add_raw_csv(data):
         writer = csv.writer(f)
         writer.writerows(data)
 
+def csv_to_text():
+    filepath = "./data.csv"
+    textpath = "./data.txt"
+    with open(filepath, "r") as my_file:
+        # pass the file object to reader()
+        file_reader = reader(my_file)
+        # do this for all the rows
+        for i in file_reader:
+            # print the rows
+            print(i[2])
+            with open(textpath, 'a') as ff:
+                print(i[2], file=ff)
+
 if __name__ == '__main__':
-    data = [["Title","URL","Abstract"]]
-    add_raw_csv(data)
+    # data = [["Title","URL","Abstract"]]
+    # add_raw_csv(data)
+    csv_to_text()
