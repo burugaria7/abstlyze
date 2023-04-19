6278d685b8f920afa66f3fd991dfa74dbca1a209
Re <burugaria@icloud.com>
2023-04-19 17:08:36 +0900

    chang the waiting time

--- src/scraping_from_FSAS_v3.py
+++ src/scraping_from_FSAS_v3.py
@@ -21,9 +21,9 @@ def fsas():
     time.sleep(1 + random.random())
     driver.find_element(By.XPATH,
                         "//*[@id=\"aa-expand-all-articles-previews\"]/div/label/span[1]").click()
-    time.sleep(2 + random.random())
+    time.sleep(2 + random.random() + random.random())
 
-    for i in range(200):
+    for i in range(2000):
         # driver.find_element(By.XPATH,
         #                     "//*[@id=\"react-root\"]/div/div/div/main/section[1]/div/div/nav/div[1]/a").click()
         print("--------------------------------------------------------------------------------")
@@ -35,12 +35,14 @@ def fsas():
                                       "//*[@id=\"react-root\"]/div/div/div/main/section[1]/div/div/nav/div[1]/a").get_attribute(
                 'href')
             driver.get(url)
-            time.sleep(1 + random.random())
+            time.sleep(1 + random.random() + random.random())
+
         print(url)
 
+        # ここでAbstract全表示を有効にする
         driver.find_element(By.XPATH,
                             "//*[@id=\"aa-expand-all-articles-previews\"]/div/label/span[1]").click()
-        time.sleep(2 + random.random())
+        time.sleep(2 + random.random() + random.random())
 
         # クラスがとたまにバグるからXPATHで指定
         # info = driver.find_elements(By.CLASS_NAME, "js-special-issue-title")
@@ -52,7 +54,7 @@ def fsas():
             jj += j.text.replace("\n", " ")
             # print("URL", j.get_attribute("href"))
         print("-  -  -  -  -  -  -  -")
-        time.sleep(1 + random.random())
+        time.sleep(1 + random.random() + random.random())
 
         # 書く記事のツリーをリストとして取得
         articles_tree = driver.find_elements(By.CLASS_NAME, "js-article.article-content")
@@ -65,9 +67,10 @@ def fsas():
                 print("No abstract ^^ continue")
                 continue
             else:
-                title = articles_tree[j].find_element(By.CLASS_NAME, "js-article-title").text
+                title = articles_tree[j].find_element(By.CLASS_NAME, "js-article-title").text.replace("\n", " ")
                 author = articles_tree[j].find_element(By.CLASS_NAME,
-                                                       "text-s.u-clr-grey8.js-article__item__authors").text
+                                                       "text-s.u-clr-grey8.js-article__item__authors").text.replace(
+                    "\n", " ")
                 abst = articles_tree[j].find_element(By.CLASS_NAME, "js-abstract-body-text.branded").find_element(
                     By.TAG_NAME, "p").text.replace("\n", " ")
                 url_ = articles_tree[j].find_element(By.CLASS_NAME,
@@ -83,6 +86,8 @@ def fsas():
                 # data = [[title], [author], [abst], [url_], [jj], [url]]
                 data = [title, author, abst, url_, jj, url]
                 csv_parser.add_raw_csv_with_filepath(data, "fsas.csv")
+
+                time.sleep(1 + random.random())
 
 
 if __name__ == "__main__":
